d1a1836a4036415f79896ed43353d479bb8eca11
Yuuta Kinoshi <pe8449510@gmail.com>
2024-12-31 16:05:22 -0300

    strinfinit_v01_invite

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Invite from './components/pages/Invite'
+
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/invitation" element={<Invite />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
